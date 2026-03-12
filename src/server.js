e00831a94ce84bb57c02c420d5b035bc6615de63
Jasmine Guinee <jasmineguinee6@gmail.com>
2026-03-12 17:05:53 +0000

    updating server.js

--- src/server.js
+++ src/server.js
@@ -1,24 +1,28 @@
+import Inert from "@hapi/inert";
+import Vision from "@hapi/vision";
 import Hapi from "@hapi/hapi";
 import Cookie from "@hapi/cookie";
-import Vision from "@hapi/vision";
-import Handlebars from "handlebars";
-import jwt from "hapi-auth-jwt2";
-import path from "path";
-import { fileURLToPath } from "url";
 import dotenv from "dotenv";
+import path from "path";
 import Joi from "joi";
-import Inert from "@hapi/inert";
+import jwt from "hapi-auth-jwt2";
 import HapiSwagger from "hapi-swagger";
-import { validate } from "./api/jwt-utils.js";
+import { fileURLToPath } from "url";
+import Handlebars from "handlebars";
 import { webRoutes } from "./web-routes.js";
 import { db } from "./models/db.js";
 import { accountsController } from "./controllers/accounts-controller.js";
+import { validate } from "./api/jwt-utils.js";
 import { apiRoutes } from "./api-routes.js";
-
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 const result = dotenv.config();
+if (result.error) {
+  console.log(result.error.message);
+
+}
 
 const swaggerOptions = {
   info: {
@@ -29,25 +33,20 @@ const swaggerOptions = {
     jwt: {
       type: "apiKey",
       name: "Authorization",
-      in: "header"
-    }
+      in: "header",
+    },
   },
-  security: [{ jwt: [] }]
+  security: [{ jwt: [] }],
 };
-
-if (result.error) {
-  console.log(result.error.message);
-}
 
 async function init() {
   const server = Hapi.server({
-    port: 3000,
-    host: "localhost",
+    port: process.env.PORT || 3000,
   });
-  await server.register(Vision);
+
   await server.register(Cookie);
-  await server.register(Inert);
   await server.register(jwt);
+
   await server.register([
     Inert,
     Vision,
@@ -71,7 +70,7 @@ async function init() {
     isCached: false,
   });
 
-   server.auth.strategy("session", "cookie", {
+  server.auth.strategy("session", "cookie", {
     cookie: {
       name: process.env.cookie_name,
       password: process.env.cookie_password,
@@ -80,13 +79,11 @@ async function init() {
     redirectTo: "/",
     validate: accountsController.validate,
   });
-
-   server.auth.strategy("jwt", "jwt", {
+  server.auth.strategy("jwt", "jwt", {
     key: process.env.cookie_password,
     validate: validate,
     verifyOptions: { algorithms: ["HS256"] },
   });
-
   server.auth.default("session");
 
   db.init("mongo");
